62562f434497ce40d3a60324624dbd5ab67d01a7
Pavel Kriz <pavel.kriz.cz@gmail.com>
2016-04-12 20:23:51 +0200

    server-side adjustments (filters on the couchbase data, pom deps)

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Copy config.properties.sample into a local config.properties and adjust it in order to allow the application to load the live data from the server.
 
 See Run class to tweak test(s) and choose a test its result is to be displayed.
+
+Run `mvn compile war:war` to make the WAR (see LocationServlet).
--- src/main/java/cz/uhk/fim/beacon/web/LocationServlet.java
+++ src/main/java/cz/uhk/fim/beacon/web/LocationServlet.java
@@ -1,7 +1,11 @@
 package cz.uhk.fim.beacon.web;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import javax.servlet.ServletException;
@@ -16,10 +20,13 @@ import cz.uhk.fim.beacon.data.Measurement;
 import cz.uhk.fim.beacon.data.Position;
 import cz.uhk.fim.beacon.estimator.WKNNPositionEstimator;
 import cz.uhk.fim.beacon.ssdistance.SignalSpaceDistanceCalculator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @WebServlet("/api/location")
 public class LocationServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	final static Logger logger = LoggerFactory.getLogger(LocationServlet.class);
        
     public LocationServlet() {
         super();
@@ -28,11 +35,22 @@ public class LocationServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String json;
 		if (request.getParameter("fingerprint") != null) {
-			DataProvider dataProvider = new FileDataProvider(getServletContext().getRealPath("/WEB-INF/couchdump.json"));
+			logger.info("fingerprint received, raw JSON: {}", request.getParameter("fingerprint"));
+			String chouchdumpFilePath = getServletContext().getRealPath("/WEB-INF/couchdump.json");
+			logger.info("loading couchdump from: {}", chouchdumpFilePath);
+			DataProvider dataProvider = new FileDataProvider(chouchdumpFilePath);
 	        List<Measurement> measurements = dataProvider.getMeasurements();
-	        List<Measurement> measurementsFiltered = measurements.stream().filter(m ->
-	        	"J3NP".equals(m.getLevel())
-	        ).collect(Collectors.toList());
+			Predicate<Measurement> defaultFilter = m ->
+				(m.getDateTime().format(DateTimeFormatter.ISO_LOCAL_DATE).equals("2016-02-23") && m.getDateTime().isAfter(LocalDateTime.of(2016, 2, 23, 18, 15))) // Krizovi paralelne
+				||
+				m.getDateTime().format(DateTimeFormatter.ISO_LOCAL_DATE).equals("2016-02-26") // J3NP  100ms Tx 0dbm
+			;
+			if (request.getParameter("sandbox") == null) {
+				// we don't want sandbox
+				defaultFilter = defaultFilter.and(m -> !"Piskoviste".equals(m.getLevel()));
+			}
+			List<Measurement> measurementsFiltered = measurements.stream().filter(defaultFilter).collect(Collectors.toList());
+			logger.info("couchbase contains {} measuments (after optional filtering)", measurementsFiltered.size());
 
 	        SignalSpaceDistanceCalculator signalSpaceDistanceCalculator = new SignalSpaceDistanceCalculator(-105);
 	        
@@ -40,9 +58,12 @@ public class LocationServlet extends HttpServlet {
 	            return signalSpaceDistanceCalculator.calcDistance(measurement1.getReducedCombinedScans(), measurement2.getReducedCombinedScans());
 	        }, 2);
 	             
-	       Measurement unknown = new Measurement().fromJson(request.getParameter("fingerprint")); 
-	       Position position = positionEstimator.estimatePosition(measurementsFiltered, unknown);
-	       json = position != null ? "{\"floor\":\"" + position.getFloor() + "\",\"x\":" + (int) position.getX() + ",\"y\":" + (int) position.getY() + "}" : "{\"floor\":\"Unknown\",\"x\":-1,\"y\":-1}";
+	       	Measurement unknown = new Measurement().fromJson(request.getParameter("fingerprint"));
+			logger.info("fingerprint converted to the object: {}", unknown);
+	       	Position position = positionEstimator.estimatePosition(measurementsFiltered, unknown);
+			logger.info("estimated position object: {}", position);
+	       	json = position != null ? "{\"floor\":\"" + position.getFloor() + "\",\"x\":" + (int) position.getX() + ",\"y\":" + (int) position.getY() + "}" : "{\"floor\":\"Unknown\",\"x\":-1,\"y\":-1}";
+			logger.info("localization result: {}", json);
 		} else json = "{\"floor\":\"Unknown\",\"x\":-1,\"y\":-1}";			
 		response.getWriter().append(json);
 	}
